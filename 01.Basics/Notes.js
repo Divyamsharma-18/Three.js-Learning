2dbcdf277013c5097f7e40a61fc014a0788cf1a5
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-01-10 02:21:52 +0530

    Update Notes.js

--- 01.Basics/Notes.js
+++ 01.Basics/Notes.js
@@ -15,4 +15,12 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5; // Move the camera back to see things better
 
 
-Basically we go with 75. Low FOV i.e. 30 will make the cube zoom in and look very big like binoculars zoom things and high FOV like  120 will zoom it out and show a wide angle.
+👀 FOV => Basically we go with 75. Low FOV i.e. 30 will make the cube zoom in and look very big like binoculars zoom things and high FOV like  120 will zoom it out and show a wide angle.
+
+ 
+
+⬜ Aspect Ratio: window.innerWidth / window.innerHeight =>
+The aspect ratio tells the camera how wide or tall the screen is.  It's like saying: "Hey camera, the screen shape is like a rectangle!"
+If you don't set this correctly, your world might look squished or stretched. 🤪
+
+
